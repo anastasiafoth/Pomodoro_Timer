705c0668ff6267142fd812fd693a5761194bad7b
anastasiafoth <anastasiafoth9@gmail.com>
2026-04-14 10:55:43 +0200

    feat: integrate Tailwind CSS into the project
    - Updated index.html to include Tailwind CSS classes for styling.
    - Modified package.json and package-lock.json to add Tailwind CSS and its Vite plugin as dependencies.
    - Refactored App.tsx to utilize Tailwind CSS for improved UI design.
    - Created index.css to import Tailwind CSS styles.
    - Updated vite.config.ts to include Tailwind CSS plugin in Vite configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [time, setTime] = useState(1500); // 25 Minuten = 1500 Sekunden
+  const [time, setTime] = useState(1500); // 25 minutes = 1500 seconds
   const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
@@ -24,11 +24,13 @@ function App() {
   };
 
   return (
-    <>
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
       {/*<!-- CARD -->*/}
-      <main>
-        <h2>Focus!</h2>
-        <h1>{formatTime(time)}</h1>
+      <main className=" bg-white p-36 text-center rounded-4xl shadow-2xl border-b-8 border-cyan-900">
+        <h2 className="text-4xl">Focus!</h2>
+        <h1 className="text-6xl font-black p-4 text-cyan-900">
+          {formatTime(time)}
+        </h1>
 
         {/*<!-- BUTTONS -->*/}
         <section>
@@ -48,16 +50,29 @@ function App() {
               <path d="M480-80q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-440h80q0 117 81.5 198.5T480-160q117 0 198.5-81.5T760-440q0-117-81.5-198.5T480-720h-6l62 62-56 58-160-160 160-160 56 58-62 62h6q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-440q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-80Z" />
             </svg>
           </button>
-          <button aria-label="pause" onClick={() => setIsRunning(!isRunning)}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="35px"
-              viewBox="0 -960 960 960"
-              width="35px"
-            >
-              <path d="M560-200v-560h160v560H560Zm-320 0v-560h160v560H240Z" />
-            </svg>
-          </button>
+          {isRunning ? (
+            <button aria-label="pause" onClick={() => setIsRunning(!isRunning)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="35px"
+                viewBox="0 -960 960 960"
+                width="35px"
+              >
+                <path d="M560-200v-560h160v560H560Zm-320 0v-560h160v560H240Z" />
+              </svg>
+            </button>
+          ) : (
+            <button aria-label="play" onClick={() => setIsRunning(!isRunning)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="35px"
+                viewBox="0 -960 960 960"
+                width="35px"
+              >
+                <path d="M320-200v-560l440 280-440 280Z" />
+              </svg>
+            </button>
+          )}
           <button aria-label="skip" onClick={() => setTime(0)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -72,7 +87,7 @@ function App() {
         {/*<!-- BUTTONS END -->*/}
       </main>
       {/*<!-- CARD END -->*/}
-    </>
+    </div>
   );
 }
 
